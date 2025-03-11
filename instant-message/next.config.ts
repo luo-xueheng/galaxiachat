a0c907e8f5ab7e16625bd7f0a7e5ac4a1fbb0b3d
2023010877 <luo-xh23@mails.tsinghua.edu.cn>
2025-03-12 00:28:05 +0800

    [ci]:fix next.config.ts

--- instant-message/next.config.ts
+++ instant-message/next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,  // 启用 appDir 功能
   },
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: false,
   swcMinify: true,  // 可选，启用 swc 压缩
 };
