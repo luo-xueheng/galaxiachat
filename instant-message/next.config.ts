47f4d625505cc73ae9a2bd7cdf2981c0487de9c0
2023010877 <luo-xh23@mails.tsinghua.edu.cn>
2025-03-16 15:18:24 +0800

    [ci]: add destination next.config.ts

--- instant-message/next.config.ts
+++ instant-message/next.config.ts
@@ -6,6 +6,15 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: false,
   swcMinify: true,  // 可选，启用 swc 压缩
+  async rewrites() {
+    return [{
+        source: "/api/:path*",
+        // TODO Start: [Student] Change to standard backend URL
+        //destination: "http://127.0.0.1:8080/:path*",
+        destination: "https://backend-sepi.app.spring25b.secoder.net/:path*", // 重写到后端
+        // TODO End
+    }];
+}
 };
 
 module.exports = nextConfig;
