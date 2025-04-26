85e6aee865f05ca7dcfbbf18148a4214f5925abe
prueyang <166228557+prueyang@users.noreply.github.com>
2025-04-27 00:48:35 +0800

    [fix]:update image path in Dockerfile and app/

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,6 +2,6 @@ export const metadata = {
   title: "Galaxia Chat",
   description: "探索无限沟通的可能, Galaxia Chat 让你在数字星际中畅所欲言, 快速、安全、智能.",
   icons: {
-    icon: "/_next/static/logosmall.ico",
+    icon: "/_next/static/favicon.ico",
   },
 };
